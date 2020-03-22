c9e90af6a696f6b97ddbb6d47821e353ddb5f0f6
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-22 17:34:12 -0400

    done

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,56 +3,165 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
 
-        Node head;
+    private Node<T> head;
+    private int listSize = 0;
+    private int index;
 
-        private class Node {
+    private class Node<T> {
 
+        private T element;
+        private Node<T> next;
 
+        // Constructor
+        public Node(T element) {
+            this.element = element;
+            next = null;
+        }
 
-            private int element;
-            private Node next;
+        // Constructor
+        public Node(T element, Node<T> next) {
+            this.element = element;
+            this.next = next;
+        }
 
-            // Constructor
-            public Node(int element) {
-                this.element = element;
-                next = null;
-            }
-            public int getElement() {
-                return element;
-            }
+        public T getElement() {
+            return element;
+        }
 
-            public void setElement(int element) {
-                this.element = element;
-            }
+        public void setElement(T element) {
+            this.element = element;
+        }
 
-            public Node getNext() {
-                return next;
-            }
+        public Node<T> getNext() {
+            return next;
+        }
 
-            public void setNext(Node next) {
-                this.next = next;
-            }
-
-
-
-
+        public void setNext(Node<T> next) {
+            this.next = next;
         }
     }
 
+    private void incrementListSize() {
+        listSize++;
+    }
 
-//    You may not use the LinkedList or ArrayList class or any other predefined Java collections
-//    Your linked list must have a node inner class to represent each element
-//    Your linked list must have add, remove, contains, find, size, get, copy and sort methods
-//
-//    Method definitions:
-//    add -- add an element to the list
-//    remove -- remove an element (specified by numeric index) from the list
+    private void decrementListSize() {
+        listSize--;
+    }
+
+//   add -- add an element to the list
+    public void addElement(T element) {
+
+        Node<T> newNode = new Node<T>(element);
+
+        if(this.head == null) {
+            this.head = newNode;
+        }
+        else {
+            Node<T> last = head;
+            while (last.getNext() != null) {
+                last = last.getNext();
+            }
+            last.setNext(newNode);
+        }
+        incrementListSize();
+    }
+
+//   remove -- remove an element (specified by numeric index) from the list
+    public boolean removeElementByIndex(int index) {
+
+        if (index < 0 || index > getListSize() || head == null)
+            return false;
+        if (index == 0) {
+            head = head.getNext();
+            return true;
+        }
+        Node<T> previous = null;
+        Node<T> current = head;
+        for (int listIndex = 1; listIndex < index; listIndex++) {
+            previous = current.getNext();
+            current = current.getNext();
+        }
+        previous.setNext(current.getNext());
+
+        decrementListSize();
+        return true;
+    }
 //    contains -- returns true if the element is in the list, false otherwise
+    public boolean containsElement(T element) {
+        if (head == null) {
+            return false;
+        }
+        Node<T> selectedElement = head;
+        if (selectedElement.getElement().equals(element)) {
+            return true;
+        }
+        for (int i = 0; i < getListSize(); i++) {
+            selectedElement = selectedElement.getNext();
+            if (selectedElement.getElement().equals(element)) {
+            return true;
+            }
+        }
+        return false;
+    }
 //    find -- returns the element's index if it is in the list, -1 otherwise
-//    size -- returns the current size of the list
-//    get -- returns the element at the specified index
-//    copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
-//    sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
+        public int findIndexByElement(T element) {
+            if (head == null) {
+                return -1;
+            }
+           Node<T> selectedNode = head;
+            for (int i = 0; i < getListSize(); i++); {
 
+                if (selectedNode.getElement().equals(element)) {
+                    return index;
+                }
+                selectedNode = selectedNode.getNext();
+            }
+            return -1;
+        }
+
+//    size -- returns the current size of the list
+        public int getListSize() {
+            return listSize;
+}
+
+//    get -- returns the element at the specified index
+    public T getElementByIndex(int index) {
+        if (index < 0 || index > getListSize())
+            return null;
+        Node<T> selectedElement = head;
+        for (int i = 0; i < index; i++) {
+            selectedElement = selectedElement.getNext();
+        }
+        return selectedElement.getElement();
+    }
+
+//    copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+    public SinglyLinkedList<T> copySameValues(SinglyLinkedList list) {
+        SinglyLinkedList<T> copyList = new SinglyLinkedList<T>();
+        for (int i = 0; i <list.getListSize(); i++) {
+            copyList.addElement(getElementByIndex(i));
+        }
+        return copyList;
+    }
+
+//    sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
+    public void sortList() {
+        if (listSize > 1)
+        for (int i = 0; i < listSize -1; i++) {
+            Node<T> currentNode = head;
+            Node<T> nextNode = head.next;
+
+            for (int j = 0; j < listSize -i -1; j++) {
+//                if ((currentNode.element).compareTo(nextNode.element) > 0) {
+                    T temp = currentNode.element;
+                    currentNode.setElement(nextNode.element);
+                    nextNode.setElement(temp);
+                }
+                currentNode = nextNode;
+                nextNode = nextNode.next;
+            }
+        }
+    }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,50 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+
+    private static String ELEMENT1 = "Element1";
+    private static String ELEMENT2 = "Element2";
+    private static String ELEMENT3 = "Element3";
+
+    private SinglyLinkedList<String> list;
+
+    @Before
+    public void setup() {
+        list = new SinglyLinkedList<String>();
+        Assert.assertTrue(list.getListSize() == 0);
+    }
+
+    @Test
+    public void addElement() {
+        list.addElement(ELEMENT1);
+        Assert.assertTrue(list.getListSize() == 1);
+        String element = list.getElementByIndex(0);
+        Assert.assertEquals(ELEMENT1, element);
+        list.addElement(ELEMENT2);
+        Assert.assertTrue(list.getListSize() == 2);
+
+    }
+
+    @Test
+    public void removeElementByIndex() {
+        Assert.assertTrue(list.getListSize() == 0);
+        boolean removeSuccess = list.removeElementByIndex(-1);
+        Assert.assertFalse(removeSuccess);
+        removeSuccess = list.removeElementByIndex(1000);
+        Assert.assertFalse(removeSuccess);
+        removeSuccess = list.removeElementByIndex(0);
+        Assert.assertFalse(removeSuccess);
+
+        list.addElement(ELEMENT1);
+
+    }
+
 }
